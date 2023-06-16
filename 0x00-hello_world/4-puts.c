4baf6aa882a64a43535bbc372d9848ea8e536385
Maryam585 <abdulrasaqmaryam585@gmail.com>
2023-06-16 14:54:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
- * Return 0.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -11,4 +11,6 @@ int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
 	return(0);
+
 }
+
