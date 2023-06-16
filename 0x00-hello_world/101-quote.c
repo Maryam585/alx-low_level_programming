f494f3aa0572dd95d92b06d8ce5fe27031211f1d
Maryam585 <abdulrasaqmaryam585@gmail.com>
2023-06-16 16:41:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
  * followed by a newline, to standard error.
  * Return: Always 1.
  */
