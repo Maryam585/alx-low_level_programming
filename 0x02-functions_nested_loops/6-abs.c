2046c11eac1970bccb132bafe3ca2e1e9dba0093
Maryam585 <abdulrasaqmaryam585@gmail.com>
2023-06-21 09:00:30 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,17 +7,16 @@
  * Return: Absolute value of number or zero
  */
 
-     int _abs(int c)
+int _abs(int c)
 
 {
-
-        if (c < 0)
-        {
+	if (c < 0)
+	{
 	int abs_val;
 
 	abs_val = c * -1;
 	return (abs_val);
 	}
-        return (c);
+	return (c);
 
 }
