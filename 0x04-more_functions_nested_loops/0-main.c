2d7ff0ec9dfc1f7b30810644bb2ee1b44ef7d21c
Maryam585 <abdulrasaqmaryam585@gmail.com>
2023-06-22 23:47:39 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -3,14 +3,14 @@
 
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
+
 int main(void)
 {
 char c;
 
-c = 'A';
+c = 'A'
 printf("%c: %d\n", c, _isupper(c));
 c = 'a';
 printf("%c: %d\n", c, _isupper(c));
